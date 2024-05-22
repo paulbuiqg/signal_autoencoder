dee8ce3f2c7325737ad6b17070fd7fb9eb25f47e
paul <pbuiquang@vegetalsignals.com>
2024-05-22 18:41:57 +0200

    Update

--- src/seismic_signals.py
+++ src/seismic_signals.py
@@ -1,8 +1,9 @@
 # %%
 
+import os
 import pickle
 
-import dataloading 
+import dataloading
 
 # %%
 
@@ -13,17 +14,24 @@ print(df_event)
 
 traces = []
 
+files = os.listdir('../data')
+
 for event_id in df_event['eventID']:
+    if any([event_id in f for f in files]):
+        print(f'{event_id} ok')
+        continue
     print(event_id)
     try:
         traces = dataloading.fetch_data_for_one_event(event_id)
         i, j = 0, 0
         while j + 2 < len(traces):
             tr = traces[j: j + 3]
-            with open(f'data/{event_id}_{i}.pkl', 'wb') as f:
+            with open(f'../data/{event_id}_{i}.pkl', 'wb') as f:
                 pickle.dump(tr, f)
             i += 1
             j += 3
     except Exception as e:
         print(f'Event {event_id}:', e)
         continue
+
+# %%
