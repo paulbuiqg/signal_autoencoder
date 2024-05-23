118700c7d5d9234c9a5004a4e40ea8630b2a6281
paul <pbuiquang@vegetalsignals.com>
2024-05-23 18:02:17 +0200

    Add encoding

--- src/seismic_autoencoding.py
+++ src/seismic_autoencoding.py
@@ -1,4 +1,4 @@
-"""Train the autoencoder, save the trained model."""
+"""Train the autoencoder, save the trained model, compute embeddings."""
 
 # %%
 # Imports
@@ -14,6 +14,7 @@ import modeling
 # %%
 # Dataset
 # One item: 3-channel signal of seismic waveforms from one event ("trace")
+
 print('Dataset creation...')
 try:
     events = pd.read_csv('data/events.csv')
@@ -27,25 +28,27 @@ print('')
 
 # %%
 # Dataloader
+
 print('Dataloader creation...')
 batch_size = 4
 dataloader = DataLoader(dataset,
                         batch_size=batch_size,
                         shuffle=True,
-                       collate_fn=dataloading.collate_fn)
+                        collate_fn=dataloading.collate_fn)
 m, s = dataloading.compute_signal_mean_and_std('cuda', dataloader)
 dataloader.dataset.set_mean_and_std(m, s)
 print('')
 
 # %%
 # Model, loss, optimizer
+
 print('Model creation...')
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 n_conv_channel_1 = 64
 n_conv_channel_2 = 128
 n_conv_channel_3 = 256
 lstm_hidden_size = 256
-n_lstm_layer = 2
+n_lstm_layer = 1
 model = modeling.SignalAutoencoder(3, n_conv_channel_1, n_conv_channel_2,
                                    n_conv_channel_3, lstm_hidden_size,
                                    n_lstm_layer).to(device)
@@ -55,7 +58,8 @@ optimizer = torch.optim.Adam(model.parameters(), lr=.001)
 print('')
 
 # %%
-# Training
+# Training (if no saved model)
+
 print('Training...')
 model.train_one_epoch(device, dataloader, loss_fn, optimizer)
 plt.plot(np.log(model.checkpoint['loss_history']), label='Training')
@@ -65,9 +69,20 @@ plt.legend()
 try:
     plt.savefig('viz/loss.png')
     torch.save(model.checkpoint, 'src/autoencoder.pt')
-except FileNotFoundError:  # For interactive mode in VSCode
+except FileNotFoundError:
     plt.savefig('../viz/loss.png')
     torch.save(model.checkpoint, '../src/autoencoder.pt')
 print('Model saved')
+print('')
 
 # %%
+# Encoding
+
+try:
+    checkpoint = torch.load('src/autoencoder.pt')
+except FileNotFoundError:
+    checkpoint = torch.load('../src/autoencoder.pt')
+model.load_state_dict(checkpoint['model_state_dict'])
+print('Encoding...')
+embeddings = model.train_one_epoch(device, dataloader, loss_fn, optimizer)
+# TODO: write embeddings to file
