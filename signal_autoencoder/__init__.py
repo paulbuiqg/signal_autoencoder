6fa96134aefc7c49286bad588febf010db635210
paul <pbuiquang@vegetalsignals.com>
2024-05-24 15:27:52 +0200

    Make package

---
+++ signal_autoencoder/__init__.py
@@ -0,0 +1,5 @@
+from .dataloading import collate_fn, SeismicSignals
+from .modeling import (SignalAutoencoder,
+                       SignalEncoder,
+                       SignalDecoder,
+                       sequence_l1)
