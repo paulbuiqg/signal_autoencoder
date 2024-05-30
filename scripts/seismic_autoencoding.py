083141389a4cd5e2e5eb752ee9f2f034b3002c7a
paul <pbuiquang@vegetalsignals.com>
2024-05-30 16:30:58 +0200

    Simplify

--- scripts/seismic_autoencoding.py
+++ scripts/seismic_autoencoding.py
@@ -36,6 +36,8 @@ dataloader = DataLoader(dataset,
                         shuffle=True,
                         collate_fn=dataloading.collate_fn)
 m, s = dataloading.compute_signal_mean_and_std(device, dataloader)
+m = m.detach().cpu()
+s = s.detach().cpu()
 dataloader.dataset.set_mean_and_std(m, s)
 torch.save(dataloader, 'data/dataloader.pth')
 print('Dataloader saved')
@@ -48,21 +50,21 @@ n_conv_channel_2 = config['conv_rec_net']['n_conv_channel_2']
 n_conv_channel_3 = config['conv_rec_net']['n_conv_channel_3']
 lstm_hidden_size = config['conv_rec_net']['lstm_hidden_size']
 n_lstm_layer = config['conv_rec_net']['n_lstm_layer']
-conv_rec_net = modeling.ConvRecAutoencoder(3, n_conv_channel_1,
-                                           n_conv_channel_2, n_conv_channel_3,
-                                           lstm_hidden_size, n_lstm_layer)
-conv_rec_net = conv_rec_net.to(device)
+model = modeling.ConvRecAutoencoder(3, n_conv_channel_1, n_conv_channel_2,
+                                    n_conv_channel_3, lstm_hidden_size,
+                                    n_lstm_layer)
+model = model.to(device)
 print(('The convolutional recurrent model has '
-       f'{modeling.count_parameters(conv_rec_net)} parameters'))
+       f'{modeling.count_parameters(model)} parameters'))
 loss_fn = modeling.sequence_l1
 print('')
 
 # Training
 print('Training...')
-optimizer = torch.optim.Adam(conv_rec_net.parameters(), lr=.0001)
-conv_rec_net.train_one_epoch(device, dataloader, loss_fn, optimizer)
-torch.save(conv_rec_net.checkpoint, 'scripts/model.pt')
-plt.plot(conv_rec_net.checkpoint['loss_history'])
+optimizer = torch.optim.Adam(model.parameters(), lr=.0001)
+model.train_one_epoch(device, dataloader, loss_fn, optimizer)
+torch.save(model.checkpoint, 'scripts/model.pt')
+plt.plot(model.checkpoint['loss_history'])
 plt.xlabel('Iteration')
 plt.ylabel('Loss')
 plt.savefig('viz/loss.png')
@@ -71,6 +73,6 @@ print('Models saved')
 print('')
 
 # Encoding
-embeddings = conv_rec_net.encode(device, dataloader)
+embeddings = model.encode(device, dataloader)
 torch.save(embeddings, 'data/embeddings.pt')
 print('Embeddings saved')
