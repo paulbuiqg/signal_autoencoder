e242c4a984d41ce098cb696ccef4472141ea2291
paul <pbuiquang@vegetalsignals.com>
2024-05-30 13:46:18 +0200

    Run ConvRecNet only

--- scripts/seismic_autoencoding.py
+++ scripts/seismic_autoencoding.py
@@ -30,31 +30,19 @@ print('')
 print('Dataloader creation...')
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Device:', device)
-if not os.path.isfile('data/dataloader.pth'):
-    batch_size = 4
-    dataloader = DataLoader(dataset,
-                            batch_size=batch_size,
-                            shuffle=True,
-                            collate_fn=dataloading.collate_fn)
-    m, s = dataloading.compute_signal_mean_and_std(device, dataloader)
-    dataloader.dataset.set_mean_and_std(m, s)
-    torch.save(dataloader, 'data/dataloader.pth')
-dataloader = torch.load('data/dataloader.pth')
+batch_size = 8
+dataloader = DataLoader(dataset,
+                        batch_size=batch_size,
+                        shuffle=True,
+                        collate_fn=dataloading.collate_fn)
+m, s = dataloading.compute_signal_mean_and_std(device, dataloader)
+dataloader.dataset.set_mean_and_std(m, s)
+torch.save(dataloader, 'data/dataloader.pth')
 print('Dataloader saved')
 print('')
 
 # Models, loss
 print('Model creation...')
-n_conv_channel_1 = config['conv_net']['n_conv_channel_1']
-n_conv_channel_2 = config['conv_net']['n_conv_channel_2']
-n_conv_channel_3 = config['conv_net']['n_conv_channel_3']
-n_conv_channel_4 = config['conv_net']['n_conv_channel_4']
-conv_net = modeling.ConvAutoencoder(3, n_conv_channel_1, n_conv_channel_2,
-                                    n_conv_channel_3, n_conv_channel_4)
-conv_net = conv_net.to(device)
-print(('The convolutional model has '
-       f'{modeling.count_parameters(conv_net)} parameters'))
-#
 n_conv_channel_1 = config['conv_rec_net']['n_conv_channel_1']
 n_conv_channel_2 = config['conv_rec_net']['n_conv_channel_2']
 n_conv_channel_3 = config['conv_rec_net']['n_conv_channel_3']
@@ -66,56 +54,23 @@ conv_rec_net = modeling.ConvRecAutoencoder(3, n_conv_channel_1,
 conv_rec_net = conv_rec_net.to(device)
 print(('The convolutional recurrent model has '
        f'{modeling.count_parameters(conv_rec_net)} parameters'))
-#
 loss_fn = modeling.sequence_l1
 print('')
 
 # Training
-print('Training convolutional neural network...')
-if not (os.path.isfile('data/dataloader.pth')
-        and os.path.isfile('scripts/conv_net.pt')):
-    optimizer = torch.optim.Adam(conv_net.parameters(), lr=.001)
-    conv_net.train_one_epoch(device, dataloader, loss_fn, optimizer)
-    torch.save(conv_net.checkpoint, 'scripts/conv_net.pt')
-checkpoint = torch.load('scripts/conv_net.pt',
-                        map_location=torch.device(device))
-plt.plot(checkpoint['loss_history'])
+print('Training...')
+optimizer = torch.optim.Adam(conv_rec_net.parameters(), lr=.0001)
+conv_rec_net.train_one_epoch(device, dataloader, loss_fn, optimizer)
+torch.save(conv_rec_net.checkpoint, 'scripts/model.pt')
+plt.plot(conv_rec_net.checkpoint['loss_history'])
 plt.xlabel('Iteration')
 plt.ylabel('Loss')
-plt.savefig('viz/conv_net_loss.png')
+plt.savefig('viz/loss.png')
 plt.close()
-#
-print('Training convolutional recurrent neural network...')
-if not (os.path.isfile('data/dataloader.pth')
-        and os.path.isfile('scripts/conv_rec_net.pt')):
-    optimizer = torch.optim.Adam(conv_rec_net.parameters(), lr=.001)
-    conv_rec_net.train_one_epoch(device, dataloader, loss_fn, optimizer)
-    torch.save(conv_rec_net.checkpoint, 'scripts/conv_rec_net.pt')
-checkpoint = torch.load('scripts/conv_rec_net.pt',
-                        map_location=torch.device(device))
-plt.plot(checkpoint['loss_history'])
-plt.xlabel('Iteration')
-plt.ylabel('Loss')
-plt.savefig('viz/conv_rec_net_loss.png')
-plt.close()
-#
 print('Models saved')
 print('')
 
 # Encoding
-print('Encoding...')
-if not (os.path.isfile('data/dataloader.pth')
-        and os.path.isfile('scripts/conv_net.pt')
-        and os.path.isfile('data/conv_net_embeddings.pt')):
-    conv_net.load_state_dict(checkpoint['model_state_dict'])
-    embeddings = conv_net.encode(device, dataloader)
-    torch.save(embeddings, 'data/conv_net_embeddings.pt')
-#
-if not (os.path.isfile('data/dataloader.pth')
-        and os.path.isfile('scripts/conv_rec_net.pt')
-        and os.path.isfile('data/conv_rec_net_embeddings.pt')):
-    conv_rec_net.load_state_dict(checkpoint['model_state_dict'])
-    embeddings = conv_rec_net.encode(device, dataloader)
-    torch.save(embeddings, 'data/conv_rec_net_embeddings.pt')
-#
+embeddings = conv_rec_net.encode(device, dataloader)
+torch.save(embeddings, 'data/embeddings.pt')
 print('Embeddings saved')
